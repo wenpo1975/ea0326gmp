b9416a9cdf405ba7b90c7cd41a5229c0e1fecdd5
Sereinfy <vnxscn@gmail.com>
2024-04-04 20:22:38 +0800

    初版

--- clash-core.sh
+++ clash-core.sh
@@ -4,7 +4,7 @@ mkdir -p files/etc/openclash/core
 
 # openclash 的 dev内核
 
-CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz"
+#CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz"
 
 # openclash 的 TUN内核
 # CLASH_TUN_VERSION=$(curl -sL https://github.com/vernesong/OpenClash/raw/core/master/core_version | head -n 2 | tail -n 1)
@@ -12,16 +12,16 @@ CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master
 
 # meta内核
 
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+#CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
 # 下载clash内核
 # openclash 的 内核解压
-wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
+#wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
 #wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
-wget -qO- $CLASH_META_URL | gunzip -c > files/etc/openclash/core/clash_meta
+#wget -qO- $CLASH_META_URL | gunzip -c > files/etc/openclash/core/clash_meta
 
 # 给内核权限
-chmod 777 files/etc/openclash/core/clash*
-# chmod +x,u+s /etc/openclash/core/clash*
+#chmod +x files/etc/openclash/core/clash*
+# chmod +x /etc/openclash/core/clash*
 
 # meta 要GeoIP.dat 和 GeoSite.dat
 #GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
