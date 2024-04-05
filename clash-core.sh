7e8bce411b552c03dc6d35d4fb3804cfa78caa0d
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2024-04-05 12:39:19 +0800

    Update clash-core.sh

--- clash-core.sh
+++ clash-core.sh
@@ -3,25 +3,25 @@ mkdir -p files/etc/openclash/core
 
 
 # openclash 的 dev内核
-CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz"
+#CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz"
 
 # openclash 的 TUN内核
-CLASH_TUN_VERSION=$(curl -sL https://github.com/vernesong/OpenClash/raw/core/master/core_version | head -n 2 | tail -n 1)
-CLASH_TUN_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64-$CLASH_TUN_VERSION.gz"
+#CLASH_TUN_VERSION=$(curl -sL https://github.com/vernesong/OpenClash/raw/core/master/core_version | head -n 2 | tail -n 1)
+#CLASH_TUN_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64-$CLASH_TUN_VERSION.gz"
 
 # meta内核
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+#CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
 
 # 下载clash内核
 # openclash 的 内核解压
-wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
-wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
-wget -qO- $CLASH_META_URL | gunzip -c > files/etc/openclash/core/clash_meta
+#wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
+#wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
+#wget -qO- $CLASH_META_URL | gunzip -c > files/etc/openclash/core/clash_meta
 
 # 给内核权限
 #chmod +x files/etc/openclash/core/clash*方案1 加权 失败
-# chmod +x /etc/openclash/core/clash* 方案2 不加权
-# sed -i '7s/$/\n333/' '/etc/openclash/custom/openclash_custom_firewall_rules.sh' 方案3 启动时加权
+# chmod +x /etc/openclash/core/clash* 方案2 不加权 失败
+# sed -i '7s/$/\n333/' '/etc/openclash/custom/openclash_custom_firewall_rules.sh' 方案3 失败
 
 # meta 要GeoIP.dat 和 GeoSite.dat
 #GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
