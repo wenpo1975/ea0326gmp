a5f7c43461c012f6643cf095caaa2b63772695e3
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2024-04-05 21:09:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,10 +32,10 @@ git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/l
 
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
-#rm -rf package/feeds/packages/mosdns
-#rm -rf package/feeds/packages/v2ray-geodata
+rm -rf package/feeds/packages/mosdns
+rm -rf package/feeds/packages/v2ray-geodata
 rm -rf feeds/luci/applications/luci-app-openclash
-#rm -rf package/feeds/luci/luci-app-openclash
+rm -rf package/feeds/luci/luci-app-openclash
 
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
